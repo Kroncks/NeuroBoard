ace73b1161f478178578b3a4909dda5e85c180bb
Kroncks <161207379+Kroncks@users.noreply.github.com>
2026-07-08 02:38:27 +0200

    Update config.h

--- include/config.h
+++ include/config.h
@@ -16,19 +16,19 @@
 #define NEURO_SD_DAT0 41
 
 // ================= Camera OV3660 =================
-#define NEURO_CAM_PIN_PWDN   -1
-#define NEURO_CAM_PIN_RESET  -1
-#define NEURO_CAM_PIN_XCLK    16
-#define NEURO_CAM_PIN_SIOD    4
-#define NEURO_CAM_PIN_SIOC    5
-#define NEURO_CAM_PIN_D7      15
-#define NEURO_CAM_PIN_D6      17
-#define NEURO_CAM_PIN_D5      18
-#define NEURO_CAM_PIN_D4      9
-#define NEURO_CAM_PIN_D3      11
-#define NEURO_CAM_PIN_D2      13
-#define NEURO_CAM_PIN_D1      12
-#define NEURO_CAM_PIN_D0      10
-#define NEURO_CAM_PIN_VSYNC   6
-#define NEURO_CAM_PIN_HREF    7
-#define NEURO_CAM_PIN_PCLK    8
+#define NEURO_CAM_PWDN   -1
+#define NEURO_CAM_RESET  -1
+#define NEURO_CAM_XCLK    16
+#define NEURO_CAM_SIOD    4
+#define NEURO_CAM_SIOC    5
+#define NEURO_CAM_D7      15
+#define NEURO_CAM_D6      17
+#define NEURO_CAM_D5      18
+#define NEURO_CAM_D4      9
+#define NEURO_CAM_D3      11
+#define NEURO_CAM_D2      13
+#define NEURO_CAM_D1      12
+#define NEURO_CAM_D0      10
+#define NEURO_CAM_VSYNC   6
+#define NEURO_CAM_HREF    7
+#define NEURO_CAM_PCLK    8
