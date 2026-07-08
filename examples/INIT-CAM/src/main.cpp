9b6ed6ec6aa55541769580511c77c47728e0527c
Kroncks <161207379+Kroncks@users.noreply.github.com>
2026-07-08 03:18:32 +0200

    init cam

---
+++ examples/INIT-CAM/src/main.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include <NeuroBoard.h>
+
+
+void setup()
+{
+    Serial.begin(115200);
+
+
+    NeuroBoard.begin();
+
+
+    if(NeuroBoard.initCamera())
+    {
+        NeuroBoard.setRGB(0,255,0);
+    }
+    else
+    {
+        NeuroBoard.setRGB(255,0,0);
+    }
+
+}
+
+
+void loop()
+{
+
+}
