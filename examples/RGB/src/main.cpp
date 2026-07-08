5d6b7e346663cd80bcad7e2d5a4252a9b62ada4a
Kroncks <161207379+Kroncks@users.noreply.github.com>
2026-07-08 02:44:17 +0200

    example RGB

---
+++ examples/RGB/src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include <NeuroBoard.h>
+
+
+void setup()
+{
+  NeuroBoard.begin();
+}
+
+
+void loop()
+{
+    NeuroBoard.setRGB(255,0,0);
+    delay(1000);
+
+    NeuroBoard.setRGB(0,255,0);
+    delay(1000);
+
+    NeuroBoard.setRGB(0,0,255);
+    delay(1000);
+}
